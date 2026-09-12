343d23aa7c3e555e637a40c49a89f7fdccd078c3
abhinav <abhinavpadige06@gmail.com>
2026-09-12 12:33:14 +0530

    Add Django manage.py

---
+++ backend/day2/django_blog/manage.py
@@ -0,0 +1,21 @@
+"""
+Django Blog API using Django REST Framework.
+Implements CRUD operations for a Post model.
+"""
+
+# backend/day2/django_blog/manage.py
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
