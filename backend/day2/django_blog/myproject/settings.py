4840c3ab316d7cfcddb78f8174db86d91f0eb358
abhinav <abhinavpadige06@gmail.com>
2026-09-12 12:31:52 +0530

    Add Django settings

---
+++ backend/day2/django_blog/myproject/settings.py
@@ -0,0 +1,47 @@
+"""
+Django Blog API using Django REST Framework.
+Implements CRUD operations for a Post model.
+"""
+
+# backend/day2/django_blog/myproject/settings.py
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-#^&*()_+=-0987654321qwertyuiopasdfghjklzxcvbnm'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'blog',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'myproject.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+USE_TZ = True
+STATIC_URL = 'static/'
