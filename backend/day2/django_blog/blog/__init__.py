d5f86211884cc09a3a107630f933d6e993aea913
abhinav <abhinavpadige06@gmail.com>
2026-09-12 12:32:52 +0530

    Add Django blog init file

---
+++ backend/day2/django_blog/blog/__init__.py
@@ -0,0 +1,7 @@
+"""
+Django Blog API using Django REST Framework.
+Implements CRUD operations for a Post model.
+"""
+
+# backend/day2/django_blog/blog/__init__.py
+default_app_config = 'blog.apps.BlogConfig'
